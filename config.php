104921f15908a0e0bbf414f8da15501763e940c6
Yuki Mazda <yuki777@gmail.com>
2011-10-20 10:39:46 +0900

    add sources.

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('CONSUMER_KEY', 'pfIjaKyXLS3fErXsVi7ayw');
+define('CONSUMER_SECRET', 'bulusc1zR4UWcX1bFAg6YX7mQfpk6w7BnPYe51qBqsA');
+define('OAUTH_CALLBACK', 'http://bundoki.coz.jp/callback.php');
+
+//define('DEBUG', 1);
+define('DEBUG', 0);
+
+define('POSITIVE_DICT', '/var/www/bundoki.coz.jp/data/positive.dic.6');
+define('NEGATIVE_DICT', '/var/www/bundoki.coz.jp/data/negative.dic.6');
