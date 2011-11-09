e557bf2c0d2f92392dab9efb4ae908f1d3820a60
Yuki Mazda <yuki777@gmail.com>
2011-11-10 08:03:12 +0900

    live : ini_set('display_errors', 0);

--- configs/live.php
+++ configs/live.php
@@ -5,4 +5,4 @@ define('SITE_URL', 'http://gokibun.com');
 define('SITE_AUTHOR', '分度器ジャスティス45°');
 define('DEBUG', 0);
 define('OAUTH_CALLBACK', 'http://gokibun.com/callback.php');
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
