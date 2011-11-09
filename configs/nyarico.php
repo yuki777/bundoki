171617ddb2b2288d6ebd22e1c5b515e137bcd4b0
Yuki Mazda <yuki777@gmail.com>
2011-11-09 22:03:29 +0900

    nyarico設定を追加

---
+++ configs/nyarico.php
@@ -0,0 +1,8 @@
+<?php
+define('ENV', 'dev');
+define('SITE_NAME', 'ゴキゲン分度器dev');
+define('SITE_URL', 'http://gokibun.nyarico.com');
+define('SITE_AUTHOR', '分度器ジャスティス45°dev');
+define('DEBUG', 1);
+define('OAUTH_CALLBACK', 'http://gokibun.nyarico.com/callback.php');
+ini_set('display_errors', 1);
