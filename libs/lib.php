c606ad43983fadbe3d83b245ec11520cab8a222c
Yuki Mazda <yuki777@gmail.com>
2011-11-10 21:35:57 +0900

    変更はないけど、curl_multi版のコードをコミットしておく。

--- libs/lib.php
+++ libs/lib.php
@@ -97,10 +97,31 @@ function get_user_status_list_unofficial($timeline){
 
 
 function get_emotion_list($status_list){
+    // 都度APIコールするver
     foreach($status_list as $status){
         $list[] = get_emotion($status);
     }
     return $list;
+
+    //// curl_multiを使って一度に20APIコールするver
+    //// 結局APIサーバ側で制限しているのか、都度コールに比べて結果がバラバラなので今回は使えなかった
+    //if(! $status_list) return;
+    //$cache = get_cache('status_list.' . md5(serialize($status_list)));
+    //if($cache){
+    //    l("cache hit. get_emotion_list() return cache.");
+    //    return $cache;
+    //}
+    //foreach($status_list as $status){
+    //    $url_list[] = NAZKI_API_URL . "&text=" . urlencode($status);
+    //}
+    //$contents_list = wget_multi($url_list);
+    //foreach($contents_list as $k => $contents){
+    //    $emotion_list[] = analyze_nazki($contents['content'], $status[$k]);
+    //}
+    //
+    //l("cache NOT hit. get_emotion_list() connect NAZKI API.");
+    //set_cache('status_list.' . md5(serialize($status_list)), $emotion_list);
+    //return $emotion_list;
 }
 
 function get_emotion($status){
@@ -113,7 +134,7 @@ function get_emotion($status){
     }
 
     $url = NAZKI_API_URL . "&text=" . urlencode($status);
-    $contents = file_get_contents($url);
+    $contents = wget($url);
     $emotion = analyze_nazki($contents, $status);
 
     l("cache NOT hit. get_emotion() connect NAZKI API.");
@@ -265,6 +286,54 @@ function wget($url){
     $contents = file_get_contents($url);
     return $contents;
 }
+
+/** 
+ * http://techblog.yahoo.co.jp/cat209/api1_curl_multi/
+ * 複数URLのコンテンツ、及び通信ステータスを一括取得する。 
+ * サンプル: 
+ *   $urls = array( "http://〜", "http://〜", "http://〜" ); 
+ *   $results = getMultiContents($urls); 
+ *   print_r($results); 
+ */  
+function wget_multi( $url_list ) {  
+    if(! $url_list) return false;
+    // マルチハンドルの用意  
+    $mh = curl_multi_init();  
+  
+    // URLをキーとして、複数のCurlハンドルを入れて保持する配列  
+    $ch_list = array();  
+  
+    // Curlハンドルの用意と、マルチハンドルへの登録  
+    foreach( $url_list as $url ) {  
+        $ch_list[$url] = curl_init($url);  
+        curl_setopt($ch_list[$url], CURLOPT_RETURNTRANSFER, TRUE);  
+        curl_setopt($ch_list[$url], CURLOPT_TIMEOUT, 10);  // cURL 関数の実行にかけられる時間の最大値。
+        curl_setopt($ch_list[$url], CURLOPT_CONNECTTIMEOUT, 0);  // 接続の試行を待ち続ける秒数。0 は永遠に待ち続けることを意味します。
+        
+        curl_multi_add_handle($mh, $ch_list[$url]);  
+    }  
+  
+    // 一括で通信実行、全て終わるのを待つ  
+    $running = null;  
+    do { curl_multi_exec($mh, $running); } while ( $running );  
+  
+    // 実行結果の取得  
+    foreach( $url_list as $url ) {  
+        // ステータスとコンテンツ内容の取得  
+        $results[$url] = curl_getinfo($ch_list[$url]);  
+        $results[$url]["content"] = curl_multi_getcontent($ch_list[$url]);  
+  
+        // Curlハンドルの後始末  
+        curl_multi_remove_handle($mh, $ch_list[$url]);  
+        curl_close($ch_list[$url]);  
+    }  
+  
+    // マルチハンドルの後始末  
+    curl_multi_close($mh);  
+  
+    // 結果返却  
+    return $results;  
+}  
 
 function make_user_timeline_unofficial($status_list){
     foreach($status_list as $status){
--- proxy.php
+++ proxy.php
@@ -7,50 +7,6 @@ $mode = $_GET['mode'];
 l($screen_name);
 l($mode);
 
-if($mode == 'get_user_status_list'){
-    $timeline     = get_user_timeline_unofficial($screen_name);
-    $status_list  = get_user_status_list_unofficial($timeline);
-    //l($status_list);
-    echo json_encode($status_list);
-    exit;
-}
-
-if($mode == 'get_emotion_point'){
-    $timeline     = get_user_timeline_unofficial($screen_name);
-    $status_list  = get_user_status_list_unofficial($timeline);
-    $emotion_list = get_emotion_list($status_list);
-    $point        = get_emotion_point($emotion_list);
-    //l($emotion_list);
-    //l($point);
-    echo json_encode($point);
-    exit;
-}
-
-if($mode == 'get_message'){
-    $timeline     = get_user_timeline_unofficial($screen_name);
-    $status_list  = get_user_status_list_unofficial($timeline);
-    $emotion_list = get_emotion_list($status_list);
-    $point        = get_emotion_point($emotion_list);
-    $message      = get_message($screen_name, $point);
-    $message = json_encode(array('message' => $message));
-    //l($message);
-    echo $message;
-    exit;
-}
-
-if($mode == 'get_tweet_link'){
-    $timeline     = get_user_timeline_unofficial($screen_name);
-    $status_list  = get_user_status_list_unofficial($timeline);
-    $emotion_list = get_emotion_list($status_list);
-    $point        = get_emotion_point($emotion_list);
-    $message      = get_message($screen_name, $point);
-    $link = get_tweet_link($message);
-    $link = json_encode(array('link' => $link));
-    //l($link);
-    echo $link;
-    exit;
-}
-
 if($mode == 'get_data'){
     $timeline     = get_user_timeline_unofficial($screen_name);
     $status_list  = get_user_status_list_unofficial($timeline);
